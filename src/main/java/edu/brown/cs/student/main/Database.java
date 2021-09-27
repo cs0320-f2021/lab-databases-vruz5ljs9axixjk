f14b43b0e4179278e1fcd4f9306d3705ad1f6b12
xrayd <72351211+xrayd@users.noreply.github.com>
2021-09-27 13:00:48 -0400

    feat: added similar REPL commands

--- src/main/java/edu/brown/cs/student/main/Database.java
+++ src/main/java/edu/brown/cs/student/main/Database.java
@@ -1,9 +1,11 @@
 package edu.brown.cs.student.main;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,11 +31,27 @@ public class Database {
    * @throws SQLException if an error occurs in any SQL query.
    */
   Database(String filename) throws SQLException, ClassNotFoundException {
-
-    /*
-     * TODO: Initialize the database connection, turn foreign keys on,
-     *  and then create the word and corpus tables if they do not exist.
-     */
+    // this line loads the driver manager class, and must be
+    // present for everything else to work properly
+    Class.forName("org.sqlite.JDBC");
+    String urlToDB = "jdbc:sqlite:" + filename;
+    conn = DriverManager.getConnection(urlToDB);
+    // these two lines tell the database to enforce foreign keys during operations, and should be present
+    Statement stat = conn.createStatement();
+    stat.executeUpdate("PRAGMA foreign_keys=ON;");
+    PreparedStatement prep;
+    prep = conn.prepareStatement("CREATE TABLE IF NOT EXISTS word("
+        + "corpus_id INTEGER,"
+        + "word TEXT,"
+        + "PRIMARY KEY (corpus_id),"
+        + "FOREIGN KEY (corpus_id) REFERENCES corpus(id)"
+        + "ON DELETE CASCADE ON UPDATE CASCADE);"
+        + "CREATE TABLE IF NOT EXISTS corpus("
+        + "id INTEGER,"
+        + "filename TEXT,"
+        + "PRIMARY KEY (id), ON DELETE CASCADE ON UPDATE CASCADE);");
+    prep.executeUpdate();
+    prep.close();
   }
 
 
